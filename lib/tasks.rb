74f7da3f2e07633e9cf968891309e1da23cf16bb
Jason Miller <jason@matmon.com>
2012-11-06 14:13:30 -0600

    adding back task from a bad merge

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -1,20 +1,20 @@
 namespace :shared do
-	task :make_shared_dir do
-		run "if [ ! -d #{shared_path}/files ]; then mkdir #{shared_path}/files; fi"
-	end
-	task :make_symlinks do
-		run "if [ ! -h #{release_path}/shared ]; then ln -s #{shared_path}/files/ #{release_path}/shared; fi"
-		run "for p in `find -L #{release_path} -type l`; do t=`readlink $p | grep -o 'shared/.*$'`; #{try_sudo} mkdir -p #{release_path}/$t; done"
+  task :make_shared_dir do
+    run "if [ ! -d #{shared_path}/files ]; then mkdir #{shared_path}/files; fi"
+  end
+  task :make_symlinks do
+    run "if [ ! -h #{release_path}/shared ]; then ln -s #{shared_path}/files/ #{release_path}/shared; fi"
+    run "for p in `find -L #{release_path} -type l`; do t=`readlink $p | grep -o 'shared/.*$'`; #{try_sudo} mkdir -p #{release_path}/$t; done"
     # no need to chown path
     #run "#{try_sudo} chown #{user}:www-data #{release_path}/$t; done"
-	end
+  end
 end
 
 namespace :nginx do
-	desc "Restarts nginx"
-	task :restart do
-		run "sudo /etc/init.d/nginx restart"
-	end
+  desc "Restarts nginx"
+  task :restart do
+    run "sudo /etc/init.d/nginx restart"
+  end
 end
 
 namespace :phpfpm do
@@ -25,29 +25,29 @@ namespace :phpfpm do
 end
 
 namespace :git do
-	desc "Updates git submodule tags"
-	task :submodule_tags do
-		run "if [ -d #{shared_path}/cached-copy/ ]; then cd #{shared_path}/cached-copy/ && git submodule foreach --recursive git fetch origin --tags; fi"
-	end
+  desc "Updates git submodule tags"
+  task :submodule_tags do
+    run "if [ -d #{shared_path}/cached-copy/ ]; then cd #{shared_path}/cached-copy/ && git submodule foreach --recursive git fetch origin --tags; fi"
+  end
 end
 
 namespace :memcached do
-	desc "Restarts Memcached"
-	task :restart do
-		run "echo 'flush_all' | nc localhost 11211", :roles => [:memcached]
-	end
-	desc "Updates the pool of memcached servers"
-	task :update do
+  desc "Restarts Memcached"
+  task :restart do
+    run "echo 'flush_all' | nc localhost 11211", :roles => [:memcached]
+  end
+  desc "Updates the pool of memcached servers"
+  task :update do
     unless find_servers( :roles => :memcached ).empty? then
       mc_servers = '<?php return array( "' + find_servers( :roles => :memcached ).join( ':11211", "' ) + ':11211" ); ?>'
       run "echo '#{mc_servers}' > #{current_path}/memcached.php", :roles => :memcached
     end
-	end
+  end
 end
 
 namespace :db do
-	desc "Syncs the staging database (and uploads) from production"
-	task :sync, :roles => :web  do
+  desc "Syncs the staging database (and uploads) from production"
+  task :sync, :roles => :web  do
     if stage != :staging then
       puts "[ERROR] You must run db:sync from staging with cap staging db:sync"
     else
@@ -81,6 +81,15 @@ namespace :db do
         puts "PRODUCTION SYNC PATH: #{production_deploy_to}/shared/files/"
         puts "running rsync"
         system "rsync -avz --delete #{production_deploy_to}/shared/files/ #{server}:#{shared_path}/files/"
-		end
-	end
+      end
+    end
+  end
+  desc "Sets the database credentials (and other settings) in wp-config.php"
+  task :make_config do
+    staging_domain ||= ''
+    {:'%%WP_STAGING_DOMAIN%%' => staging_domain, :'%%WP_STAGE%%' => stage, :'%%DB_NAME%%' => wpdb[stage][:name], :'%%DB_USER%%' => wpdb[stage][:user], :'%%DB_PASSWORD%%' => wpdb[stage][:password], :'%%DB_HOST%%' => wpdb[stage][:host]}.each do |k,v|
+      #FIXME complicated passwords will break sed as they are not escaped. 
+      run "sed -i 's/#{k}/#{v}/' #{release_path}/wp-config.php", :roles => :web
+    end
+  end
 end
